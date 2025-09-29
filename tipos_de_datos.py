cb4019cc5d56530c76810f129dd780f496998a60
alansastre <alan@certidevs.com>
2025-09-29 13:59:54 +0200

    tipos de datos

---
+++ tipos_de_datos.py
@@ -0,0 +1,35 @@
+# tipos de datos numéricos
+age = 20
+height = 1.80
+price = 4.99
+print(price)
+
+
+# tipos de datos texto
+nombre = 'Alan'
+apellido = "Sastre"
+telefono_movil = '666555444'
+print(nombre + apellido)
+print(nombre + " " + apellido)
+
+# tipos de datos boolean
+is_student = True
+is_eligible = False
+
+calificacion = 9
+
+# Convenciones de nomenclatura:
+# no empiecen por número
+# formato snake_case
+# no tenga espacios
+# evitar caracteres especiales: ñ, acentos, símbolos
+# si es constante lo solemos poner en MAYUSCULAS
+DESCUENTO = 10
+PI = 3.14
+IVA = 21
+DEFAULT_USERNAME = 'User'
+
+calificacion = 9
+
+telefono_movil = '666555444'
+

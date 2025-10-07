d27eb6c687b31629eeb4042a6f8aa4e68e294f2d
alansastre <alan@certidevs.com>
2025-10-07 09:21:01 +0200

    Reestructuración tipos de datos

---
+++ 001.tipos_de_datos/string_format.py
@@ -0,0 +1,21 @@
+
+"""
+Formatear cadenas de texto strings para introducir datos de variables en los string
+
+Muy útil para aplicaciones de consola y para logging
+
+Día: martes 07/10/2025 09:12
+"""
+
+# con una variable
+precio = 55
+descripcion = "El producto vale {} €"
+print(descripcion.format(precio))
+
+
+# con más de una variable
+cantidad = 5
+id = 34
+precio = 54
+descripcion = "Compra de {} piezas de producto con id {} a precio {:.2f} €"
+print(descripcion.format(cantidad, id, precio))
---
+++ 001.tipos_de_datos/tipos_de_datos.md
@@ -0,0 +1,8 @@
+
+Resumen:
+
+* int
+* float
+* str
+* boolean
+* Módulo random para crear números aleatorios

e5b9ca45031cc0091b94553643cf00b1dfb1462b
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-10-13 12:21:14 +0200

    listar ordenar

---
+++ 007.estructuras_datos/001.listas/listar_ordenar.py
@@ -0,0 +1,25 @@
+numeros = [3, 1, 4, 2]
+print(numeros)
+numeros.sort()
+print(numeros)  # [1, 2, 3, 4]
+
+# Orden descendente
+numeros.sort(reverse=True)
+print(numeros)  # [4, 3, 2, 1]
+
+print()
+
+# Ordenar cadenas
+palabras = ["zebra", "avión", "casa"]
+print(palabras)
+palabras.sort()
+print(palabras)  # ["avión", "casa", "zebra"]
+
+print()
+
+letras = ["a", "b", "c", "e"]
+print(letras)
+letras.reverse()
+print(letras)  # ["e", "c", "b", "a"]
+
+print()

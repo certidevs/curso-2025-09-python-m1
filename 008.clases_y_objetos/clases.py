e888bbaeea4d8403064fd43cf789e89ca173e1ae
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-10-14 11:54:25 +0200

    creación clases

---
+++ 008.clases_y_objetos/clases.py
@@ -0,0 +1,13 @@
+"""
+Una clase es una plantilla o plano que define:
+
+- Características (atributos): color, precio, raza, altura, id
+- Comportamientos (métodos): arrancar, ladrar, iniciar sesión
+"""
+
+class Coche:
+    # definimos atributos y métodos
+    pass
+
+mi_coche = Coche()
+coche_de_amigo = Coche()

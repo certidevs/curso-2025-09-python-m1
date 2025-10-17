4fab4212863f607f30505c1999a1e62003bb20bd
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-10-17 14:04:04 +0200

    resumen relaciones composición clases

---
+++ 008.clases_y_objetos/composicion.py
@@ -0,0 +1,27 @@
+"""
+Uno a Uno (One to One):
+    Un ciudadano español tiene UN DNI. Y ese DNI pertenece sólo a UN ciudadano español.
+"""
+
+
+
+"""
+Uno a Muchos (One to Many):
+    Una categoría puede tener MUCHOS productos. Y un producto sólo puede pertenecer a UNA categoría.
+"""
+
+
+
+"""
+Muchos a Uno (Many to One):
+    Un profesor trabaja en UN departamento. Y ese departamento puede tener MUCHOS profesores.
+"""
+
+
+
+"""
+Muchos a muchos (Many to Many):
+    Un estudiante pueden tener MUCHAS asignaturas. Cada asignatura puede tener MUCHOS estudiantes.
+"""
+
+
